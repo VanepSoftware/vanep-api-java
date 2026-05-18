de51b0b63fc35c0502c43080ef14b22101519e5d
CaiooLucas <caio.borges210805@gmail.com>
2026-05-18 14:14:09 -0300

    Add: Added validations to atributes

--- src/main/java/br/com/vanep/users/SignUpDataUsers.java
+++ src/main/java/br/com/vanep/users/SignUpDataUsers.java
@@ -1,5 +1,29 @@
 package br.com.vanep.users;
 
-public record SignUpDataUsers(String names, String email, String username, String passwords, String cpf, String phone, Integer address_id,  Type_user types) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record SignUpDataUsers(
+
+    @NotBlank
+    String names,
+    @NotBlank
+    @Email 
+    String email, 
+    @NotBlank
+    String username, 
+    String passwords,
+    @NotBlank
+    @Pattern(regexp = "\\d{11}") 
+    String cpf, 
+    @NotBlank
+    @Pattern(regexp = "\\d{11}") 
+    String phone, 
+    @NotNull
+    Integer address_id,  
+    @NotNull
+    Type_user types) {
 
 }
