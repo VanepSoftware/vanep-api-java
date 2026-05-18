fabb20680ae859cbf46b4b1e847693c6db7e2d9b
CaiooLucas <caio.borges210805@gmail.com>
2026-05-18 14:16:43 -0300

    fix: Removed comments and add more space to varchar

--- src/main/resources/db/migration/V2__create-table-users.sql
+++ src/main/resources/db/migration/V2__create-table-users.sql
@@ -1,15 +1,14 @@
--- 1. Criação da tabela 'users'
 CREATE TABLE
     users (
         id BIGSERIAL PRIMARY KEY,
-        token VARCHAR(100) UNIQUE,
+        token VARCHAR(200) UNIQUE,
         types VARCHAR(20) NOT NULL,
         names VARCHAR(150) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         username VARCHAR(50) NOT NULL UNIQUE,
-        passwords VARCHAR(255) NOT NULL,
-        cpf VARCHAR(14) NOT NULL UNIQUE,
-        phone VARCHAR(20),
+        password_hash VARCHAR(255) NOT NULL,
+        cpf VARCHAR(25) NOT NULL UNIQUE,
+        phone VARCHAR(30),
         address_id INT NULL,
         created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
