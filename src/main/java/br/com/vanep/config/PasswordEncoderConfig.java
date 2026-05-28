f70515cacd52c3687bf3ff32687d899356824c88
joao <joaovictoralvesbittencourt@gmail.com>
2026-05-28 01:17:43 -0300

    fix(VAN-31): app  properties fix

--- src/main/java/br/com/vanep/config/PasswordEncoderConfig.java
+++ src/main/java/br/com/vanep/config/PasswordEncoderConfig.java
@@ -10,8 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordEncoderConfig {
 
   @Bean
-  public PasswordEncoder passwordEncoder(
-      @Value("${vanep.security.password.pepper:}") String pepper) {
+  public PasswordEncoder passwordEncoder(@Value("${VANEP_PASSWORD_PEPPER}") String pepper) {
     PasswordEncoder argon2 = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     return new PepperedPasswordEncoder(argon2, pepper);
   }
